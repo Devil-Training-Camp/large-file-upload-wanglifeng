7be851ec5689b1ad7320a799c1865afe9319cc88
wlf1112 <2322825963@qq.com>
2024-05-16 07:12:06 +0800

    feat:add chores koa

---
+++ server/app.ts
@@ -0,0 +1,16 @@
+import Koa, { Context } from "koa";
+import logger from "koa-pino-logger";
+import bodyParser from "koa-bodyparser";
+import { errorCatch } from "./middlewares/errorCatch";
+import { HEADERS, PORT } from "./middlewares/config";
+
+const app = new Koa();
+
+// 日志
+app.use(logger);
+
+// 处理body数据
+app.use(bodyParser());
+
+// 异常处理
+app.use(errorCatch());
